7ae473628f34c17e0b7760e3f3c5386b18b5f078
Aleksey Shalynin <aleksey.shalynin@skillbox.ru>
2020-01-15 00:46:22 +0300

    Fix gh script and provide completions for it

--- scripts/gh/gh.py
+++ scripts/gh/gh.py
@@ -3,15 +3,19 @@ import os
 import click
 
 
+def get_projects(ctx, args, incomplete):
+    print(incomplete)
+    return [d for d in os.listdir(os.path.expanduser("~/github/")) if incomplete in d]
+
+
 @click.command()
-@click.argument("project", type=click.STRING)
+@click.argument("project", type=click.STRING, autocompletion=get_projects)
 def cli(project):
     os.system(f"kitty @ set-tab-title {project}")
     initial_dir = os.getcwd()
     project_dir = f"~/github/{project}"
-    os.chdir(project_dir)
+    os.chdir(os.path.expanduser(project_dir))
     click.edit(filename="./")
     # roll back to initial dir
     os.chdir(initial_dir)
     os.system("kitty @ set-tab-title use me")
-
