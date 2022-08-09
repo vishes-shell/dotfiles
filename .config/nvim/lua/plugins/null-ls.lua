1d635fdf37f8bfb2fe2055be8f2069ab1acdc465
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2022-08-09 12:52:13 +0300

    Clean null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -8,35 +8,46 @@
 local null_ls = require("null-ls")
 
 local sources = {
-  null_ls.builtins.diagnostics.codespell,
+    null_ls.builtins.diagnostics.codespell,
 
-  null_ls.builtins.diagnostics.alex,
-  null_ls.builtins.formatting.remark,
+    null_ls.builtins.diagnostics.alex,
+    -- null_ls.builtins.formatting.remark,
 
-  null_ls.builtins.formatting.isort,
-  null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
 
-  null_ls.builtins.diagnostics.vulture,
-  null_ls.builtins.diagnostics.flake8,
-  null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.vulture.with({
+        extra_args = { '--min-confidence', '90' }
+    }),
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.pylint,
 
-  null_ls.builtins.diagnostics.yamllint,
+    null_ls.builtins.diagnostics.yamllint,
 
-  null_ls.builtins.formatting.jq,
+    null_ls.builtins.formatting.jq,
 }
 
-null_ls.setup({ 
-  sources = sources,
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
-    end
-    vim.cmd [[
-      augroup document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]]
-  end
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local callback = function()
+    vim.lsp.buf.format({
+        bufnr = bufnr,
+        filter = function(client)
+            return client.name == "null-ls"
+        end
+    })
+end
+
+null_ls.setup({
+    sources = sources,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = callback,
+            })
+        end
+    end,
 })
