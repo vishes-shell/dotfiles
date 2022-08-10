fdd796836496f97884ffb3a25d68114815567520
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2022-08-10 12:18:51 +0300

    lspsaga without lspking

--- .config/nvim/lua/plugins/lsp_saga.lua
+++ .config/nvim/lua/plugins/lsp_saga.lua
@@ -6,8 +6,6 @@
 -- url: https://github.com/glepnir/lspsaga.nvim
 
 local saga = require("lspsaga")
-local kind = require('lspsaga.lspkind')
-kind[type_number][2] = icon
 
 saga.init_lsp_saga({
   code_action_lightbulb = {
