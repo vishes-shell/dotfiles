5cca5993a7dd6e0b67cddc090d4cd83f9af8259f
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2022-10-17 07:53:54 +0300

    Ruff custom null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -4,35 +4,42 @@
 -- Plugin: null-ls
 -- url: https://github.com/jose-elias-alvarez/null-ls.nvim
 local null_ls = require("null-ls")
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+local sources_diagnostics = {
+    ruff = h.make_builtin({
+        method = DIAGNOSTICS,
+        filetypes = {"python"},
+        generator_opts = {
+            command = "ruff",
+            args = {"$FILENAME"},
+            ignore_stderr = true,
+            format = "line",
+            on_output = h.diagnostics.from_pattern([[:(%d+):(%d+): (.+)]],
+                                                   {"row", "col", "message"}, {})
+        },
+        factory = h.generator_factory
+    })
+}
 
 local sources = {
-    -- common
-    --
-    -- diagnostics
     null_ls.builtins.diagnostics.codespell, null_ls.builtins.diagnostics.alex,
-
-    -- null_ls.builtins.formatting.remark,
-
-    -- python
-    --
-    -- formatting
     null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.black.with({extra_args = {"--fast"}}),
-    -- diagnostics
     null_ls.builtins.diagnostics.vulture.with({
         extra_args = {"--min-confidence", "90"}
-    }), null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.diagnostics.pylint, -- yaml
-    --
-    -- diagnostics
-    null_ls.builtins.diagnostics.yamllint, -- json
-    --
-    -- formatting
-    null_ls.builtins.formatting.jq, -- lua
-    --
-    -- formatting
+    }), -- null_ls.builtins.diagnostics.flake8,
+    -- null_ls.builtins.diagnostics.pylint, 
+    null_ls.builtins.diagnostics.yamllint, sources_diagnostics.ruff,
+    null_ls.builtins.formatting.jq,
     null_ls.builtins.formatting.lua_format.with({
-        extra_args = {"--single-quote-to-double-quote"}
+        extra_args = {
+            "--single-quote-to-double-quote", "--spaces-inside-table-braces",
+            "--extra-sep-at-table-end", "--chop-down-kv-table"
+        }
     })
 }
 
