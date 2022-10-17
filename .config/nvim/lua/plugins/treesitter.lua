cb6d54b00f6b0490782c010761e638b7b103aa86
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2022-10-17 05:46:26 +0300

    Treesitter ensure installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@
 -- url: https://github.com/nvim-treesitter/nvim-treesitter
 
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { 'python', 'markdown', 'norg' },
+  ensure_installed = { 'python', 'markdown', 'lua', 'bash', 'sql', 'yaml', 'toml', 'json' },
   sync_install = false,
   auto_install = true,
   highlight = {
