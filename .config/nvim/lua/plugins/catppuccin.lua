6401fc20abd43d63b28c9255ac6c7de7f57835da
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-01-14 10:37:31 +0300

    Format catpuccin file

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,43 +1,38 @@
 -----------------------------------------------------------
 -- Catppuccin configuration file
 -----------------------------------------------------------
-
 -- Plugin: catppuccin
 -- url: https://github.com/catppuccin/nvim
-
 vim.g.catppuccin_flavour = "latte" -- latte, frappe, macchiato, mocha
 
 require("catppuccin").setup({
-  integrations = {
-    treesitter = true,
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = { "italic" },
-        hints = { "italic" },
-        warnings = { "italic" },
-        information = { "italic" },
-      },
-      underlines = {
-        errors = { "underline" },
-        hints = { "underline" },
-        warnings = { "underline" },
-        information = { "underline" },
-      },
+    integrations = {
+        treesitter = true,
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+            },
+        },
+        cmp = true,
+        gitsigns = true,
+        indent_blankline = { enabled = true, colored_indent_levels = false },
+        ts_rainbow = true,
+        notify = true,
+        navic = true,
+        lsp_saga = true,
+        markdown = true,
+        telescope = true,
     },
-    cmp = true,
-    gitsigns = true,
-    indent_blankline = {
-      enabled = true,
-      colored_indent_levels = false,
-    },
-    ts_rainbow = true,
-    notify = true,
-    navic = true,
-    lsp_saga = true,
-    markdown = true,
-    telescope = true,
-  }
 })
 
 vim.cmd [[colorscheme catppuccin]]
