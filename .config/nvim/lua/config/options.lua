600bdeac503b51d456609793e42bc60aefbd9c62
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-01-25 02:46:54 +0300

    Setup tags

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.g.XkbSwitchEnabled = 1
+
+vim.g.gutentags_file_list_command = 'fd'
+vim.g.gutentags_ctags_extra_args = { '--languages=python', '--kinds-python=-iv' }
+vim.g.gutentags_ctags_tagfile = 'tags'
+vim.g.gutentags_file_list_command = "fd --hidden --strip-cwd-prefix --type f -E .git -E .venv"
---
+++ .config/nvim/lua/plugins/tags.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "ludovicchabant/vim-gutentags",
+  }
+}
