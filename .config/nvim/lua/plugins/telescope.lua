65d6a8eba9599ae47c0784a1ef8e31b7d3ee94f9
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-01-25 08:09:33 +0300

    Add fzf native for faster navigation

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,24 @@
+local Util = require("lazyvim.util")
+
 return {
   { "nvim-telescope/telescope.nvim",
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end
+      },
+      {
+        "xiyaowong/telescope-emoji.nvim"
+      },
     },
     keys = {
       { "<leader>tt", "<cmd>Telescope tags<cr>", desc = "Tags" },
       { "<leader>tb", "<cmd>Telescope current_buffer_tags<cr>", desc = "Tags" },
+      { "<leader>ff", Util.telescope("files", { hidden = true }), desc = "Find Files (root dir)" },
+      { "<leader>fF", Util.telescope("files", { hidden = true, cwd = false }), desc = "Find Files (cwd)" },
     },
     config = function(_, opts)
       local telescope = require("telescope")
@@ -35,12 +44,12 @@ return {
 
     }
   },
-  {
-    "xiyaowong/telescope-emoji.nvim",
-    lazy = true,
-    dependencies = "nvim-telescope/telescope.nvim",
-    config = function()
-      require("telescope").load_extension("emoji")
-    end,
-  }
+  -- {
+  --   "xiyaowong/telescope-emoji.nvim",
+  --   lazy = true,
+  --   dependencies = "nvim-telescope/telescope.nvim",
+  --   config = function()
+  --     return require("telescope").load_extension("emoji")
+  --   end,
+  -- }
 }
