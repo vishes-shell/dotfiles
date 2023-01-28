934076153f0bafc51977cad69a8276fb22a53c3b
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-01-28 14:03:10 +0300

    Add dictionary cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,12 +2,33 @@ return {
   {
     "nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-emoji",
+      { "hrsh7th/cmp-emoji" },
+      {
+        "uga-rosa/cmp-dictionary",
+        lazy = false,
+        config = function()
+          require('cmp_dictionary').setup({
+            async = true,
+            dic = {
+              ['markdown'] = {
+                vim.fn.expand("~/.config/nvim/lua/dictionaries/jira-components"),
+                vim.fn.expand("~/.config/nvim/lua/dictionaries/jira-labels")
+              }
+            },
+            debug = true,
+          })
+        end,
+      }
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      opts.sources = cmp.config.sources(vim.list_extend(
+        opts.sources,
+        {
+          { name = "emoji" },
+          { name = "dictionary", keyword_length = 2, }
+        }))
     end,
   },
 }
