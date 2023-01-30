199885f3d6545d1b162cd50e6bb076dae930e392
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-01-30 12:44:49 +0300

    Fix cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,16 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    version = false,
     dependencies = {
-      { "hrsh7th/cmp-emoji" },
+      "hrsh7th/cmp-emoji",
+      { "hrsh7th/cmp-nvim-lsp", },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
       {
         "uga-rosa/cmp-dictionary",
-        lazy = false,
+        lazy = true,
         config = function()
           require('cmp_dictionary').setup({
             async = true,
@@ -18,17 +23,49 @@ return {
             debug = false,
           })
         end,
-      }
+      },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
+    opts = function()
       local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(
-        opts.sources,
-        {
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
           { name = "emoji" },
-          { name = "dictionary", keyword_length = 2, }
-        }))
-    end,
+          -- { name = "dictionary", keyword_length = 2, }
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("lazyvim.config").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "LspCodeLens",
+          },
+        },
+      }
+    end
   },
 }
