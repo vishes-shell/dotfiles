58fb2c765aca31e96e75c3eaefdef043017417fd
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-02-05 15:05:34 +0300

    Add sqlfmt to null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,3 +1,20 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+sqlfmt = h.make_builtin({
+  method = FORMATTING,
+  filetype = { 'sql' },
+  generator_opt = {
+    command = 'sqlfmt',
+    args = {},
+    to_stdin = true,
+  },
+  factory = h.formatter_factory
+})
+
+
 return {
   {
     "jose-elias-alvarez/null-ls.nvim",
@@ -13,6 +30,7 @@ return {
           nls.builtins.formatting.black.with({ extra_args = { "--fast", "--workers", "1" } }),
           nls.builtins.formatting.jq,
           nls.builtins.formatting.yamlfmt,
+          sqlfmt,
         },
       }
     end,
