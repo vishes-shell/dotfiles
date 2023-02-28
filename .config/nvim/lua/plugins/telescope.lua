e6a149152fd462f8a77b0bf8ad86b89325d3532a
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-02-28 07:25:55 +0300

    Provide picker for find_files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -86,6 +86,12 @@ return {
           },
         },
       },
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
     },
   },
 }
