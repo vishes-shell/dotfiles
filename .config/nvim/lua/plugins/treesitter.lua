825c9313cd1dbe03b5f093eb335df88835769928
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-06 08:13:59 +0300

    Better identation with treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 return {
+  { "yioneko/nvim-yati", event = "InsertEnter" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -16,7 +17,6 @@ return {
         "lua",
         "jsonc",
         "markdown",
-        "markdown_inline",
         "python",
         "query",
         "regex",
@@ -26,9 +26,26 @@ return {
         "vim",
         "yaml",
         "json",
+        "gitcommit",
+        "rst",
       },
       highlight = { enable = true },
-      -- indent = { enable = false },
+      indent = { enable = false },
+      yati = {
+        enable = true,
+        default_lazy = true,
+        default_fallback = "auto",
+      },
+      incremental_selection = {
+        enable = true,
+        disable = {},
+        keymaps = {
+          init_selection = "<CR>", -- maps in normal mode to init the node/scope selection
+          node_incremental = "<CR>", -- increment to the upper named parent
+          scope_incremental = "<nop>", -- increment to the upper scope (as defined in locals.scm)
+          node_decremental = "<BS>", -- decrement to the previous node
+        },
+      },
       query_linter = {
         enable = true,
         use_virtual_text = true,
