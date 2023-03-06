4561f438b95aaf5cca15112cadd04da505c88696
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-06 08:13:01 +0300

    Better mapping for symbols-outline

--- .config/nvim/lua/plugins/symbols-outline.lua
+++ .config/nvim/lua/plugins/symbols-outline.lua
@@ -1,7 +1,7 @@
 return {
   {
     "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    keys = { { "|", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
     config = true,
   }
 }
