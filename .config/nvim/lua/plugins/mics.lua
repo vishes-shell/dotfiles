60c78091eebb5859add625f834ef271a105c6113
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-11 12:52:16 +0300

    Format nvim misc

--- .config/nvim/lua/plugins/mics.lua
+++ .config/nvim/lua/plugins/mics.lua
@@ -2,9 +2,7 @@ return {
   "folke/twilight.nvim",
   {
     "Wansmer/treesj",
-    keys = {
-      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-    },
+    keys = { { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" } },
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
   {
@@ -20,14 +18,4 @@ return {
       },
     },
   },
-  -- {
-  --   "shortcuts/no-neck-pain.nvim",
-  --   config = function()
-  --     require('no-neck-pain').setup({
-  --       debug = false,
-  --       width = 120,
-  --     })
-  --   end,
-  --   keys = { { "<leader>wn", "<cmd>NoNeckPain<cr>", desc = "Stop Neck pain" } },
-  -- }
 }
