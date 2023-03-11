c8b4b8f79c4e93a5e722f898d854c25444f13315
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-11 12:34:29 +0300

    Update neo-tree filesystem filtered items

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = { filesystem = { filtered_items = {
-    hide_dotfiles = false,
-    hide_hidden = false,
-  } } },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_hidden = false,
+      },
+    },
+  },
 }
