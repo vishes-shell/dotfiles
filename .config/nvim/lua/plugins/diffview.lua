31b21f79f43a5fd94f1407e8a9a28ebe58bb17b5
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-12 08:54:28 +0300

    Make use of diffview

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -3,6 +3,9 @@ return {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
     config = true,
-    keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
+    keys = {
+      { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
+      { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Close diff view" },
+    },
   },
 }
