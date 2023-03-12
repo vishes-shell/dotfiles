e051e8d9431d0a51de5c74a20707560919a98478
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-12 18:02:11 +0300

    Diffview is awesome, lets use it!

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,11 +1,29 @@
 return {
   {
     "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
     config = true,
     keys = {
       { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Open diff view" },
       { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Close diff view" },
+      { "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "File git history" },
+      {
+        "<leader>gdr",
+        function()
+          vim.ui.input({ prompt = "Target branch" }, function(branch)
+            if branch then
+              vim.cmd({
+                cmd = "DiffviewOpen",
+                args = {
+                  branch .. "...",
+                  "--imply-local",
+                },
+              })
+            end
+          end)
+        end,
+        desc = "Review changes with target branch",
+      },
     },
   },
 }
