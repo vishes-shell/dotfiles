87744f6c0b09506e59c8be3f4c22e7a3367a2edb
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-22 06:20:27 +0300

    Change default conceallevel

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+
 vim.g.XkbSwitchEnabled = 1
 
-vim.g.gutentags_file_list_command = 'fd'
-vim.g.gutentags_ctags_extra_args = { '--languages=python', '--kinds-python=-iv' }
-vim.g.gutentags_ctags_tagfile = 'tags'
+vim.g.gutentags_file_list_command = "fd"
+vim.g.gutentags_ctags_extra_args = { "--languages=python", "--kinds-python=-iv" }
+vim.g.gutentags_ctags_tagfile = "tags"
 vim.g.gutentags_file_list_command = "fd --hidden --strip-cwd-prefix --type f -E .git -E .venv"
+
+opt.conceallevel = 0
