d8827d65e652722ba955d1695590d61be88bda0b
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-29 12:12:38 +0300

    Spider for navigation?

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -19,4 +19,26 @@ return {
       },
     },
   },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    config = function()
+      local spider = require("spider")
+      vim.keymap.set({ "n", "o", "x" }, "w", function()
+        spider.motion("w")
+      end, { desc = "Spider-w" })
+
+      vim.keymap.set({ "n", "o", "x" }, "e", function()
+        spider.motion("e")
+      end, { desc = "Spider-e" })
+
+      vim.keymap.set({ "n", "o", "x" }, "b", function()
+        spider.motion("b")
+      end, { desc = "Spider-b" })
+
+      vim.keymap.set({ "n", "o", "x" }, "ge", function()
+        spider.motion("ge")
+      end, { desc = "Spider-ge" })
+    end,
+  },
 }
