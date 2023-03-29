644a0405b7fa4f239d91006f19bdc905a9ed5565
Alex Shalynin <6602362+vishes-shell@users.noreply.github.com>
2023-03-29 12:00:42 +0300

    Add advanced-git-search.nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,7 +2,7 @@ return {
   {
     "dinhhuy258/git.nvim",
     config = function()
-      require('git').setup({
+      require("git").setup({
         default_mappings = false,
       })
     end,
@@ -10,9 +10,19 @@ return {
       {
         "<leader>gb",
         "<cmd>GitBlame<cr>",
-        desc = "Git blame"
-      }
-    }
-
-  }
+        desc = "Git blame",
+      },
+    },
+  },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    config = function()
+      require("telescope").load_extension("advanced_git_search")
+    end,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- to show diff splits and open commits in browser
+      "tpope/vim-fugitive",
+    },
+  },
 }
