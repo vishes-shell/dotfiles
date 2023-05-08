42ffff2b25aa403ef626d60ceca5525d1efd3647
Aleksei Shalynin <aleksei.shalynin@Shalynin-Heart.local>
2023-05-08 11:31:21 +0300

    New config for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,7 +9,118 @@ local function font(opts)
 	})
 end
 
+local process_icons = {
+	["docker"] = {
+		{ Text = wezterm.nerdfonts.linux_docker },
+	},
+	["docker-compose"] = {
+		{ Text = wezterm.nerdfonts.linux_docker },
+	},
+	["kuberlr"] = {
+		{ Text = wezterm.nerdfonts.linux_docker },
+	},
+	["kubectl"] = {
+		{ Text = wezterm.nerdfonts.linux_docker },
+	},
+	["nvim"] = {
+		{ Text = wezterm.nerdfonts.custom_vim },
+	},
+	["vim"] = {
+		{ Text = wezterm.nerdfonts.dev_vim },
+	},
+	["node"] = {
+		{ Text = wezterm.nerdfonts.mdi_hexagon },
+	},
+	["zsh"] = {
+		{ Text = wezterm.nerdfonts.dev_terminal },
+	},
+	["bash"] = {
+		{ Text = wezterm.nerdfonts.cod_terminal_bash },
+	},
+	["btm"] = {
+		{ Text = wezterm.nerdfonts.mdi_chart_donut_variant },
+	},
+	["htop"] = {
+		{ Text = wezterm.nerdfonts.mdi_chart_donut_variant },
+	},
+	["cargo"] = {
+		{ Text = wezterm.nerdfonts.dev_rust },
+	},
+	["go"] = {
+		{ Text = wezterm.nerdfonts.mdi_language_go },
+	},
+	["lazydocker"] = {
+		{ Text = wezterm.nerdfonts.linux_docker },
+	},
+	["git"] = {
+		{ Text = wezterm.nerdfonts.dev_git },
+	},
+	["lua"] = {
+		{ Text = wezterm.nerdfonts.seti_lua },
+	},
+	["wget"] = {
+		{ Text = wezterm.nerdfonts.mdi_arrow_down_box },
+	},
+	["curl"] = {
+		{ Text = wezterm.nerdfonts.mdi_flattr },
+	},
+	["gh"] = {
+		{ Text = wezterm.nerdfonts.dev_github_badge },
+	},
+}
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane.current_working_dir
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "." or string.gsub(current_dir, "(.*[/\\])(.*)", "%2")
+end
+
+local function get_process(tab)
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	if string.find(process_name, "kubectl") then
+		process_name = "kubectl"
+	end
+
+	return wezterm.format(process_icons[process_name] or { { Text = string.format("[%s]", process_name) } })
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
+
+	local title = string.format(
+		" %s  %s ~ %s  ",
+		wezterm.nerdfonts.fa_chevron_right,
+		get_process(tab),
+		get_current_working_dir(tab)
+	)
+
+	if has_unseen_output then
+		return {
+			{ Foreground = { Color = "Orange" } },
+			{ Text = title },
+		}
+	end
+
+	return {
+		{ Text = title },
+	}
+end)
+
 return {
+	audible_bell = "Disabled",
+	canonicalize_pasted_newlines = "LineFeed",
+	cursor_blink_rate = 500,
+	cursor_thickness = 1.5,
+	default_cursor_style = "BlinkingBar",
+	default_cwd = wezterm.home_dir,
 	default_prog = { "/usr/local/bin/fish" },
 	font = wezterm.font("JetBrains Mono"),
 	font_rules = {
@@ -146,12 +257,23 @@ return {
 		{ key = "C", mods = "CTRL", action = act.CopyTo("ClipboardAndPrimarySelection") },
 		{ mods = "SUPER", key = "-", action = act.DecreaseFontSize },
 		{ mods = "SUPER", key = "+", action = act.IncreaseFontSize },
-		{ mods = "CTRL", key = "X", action = act.ActivateCopyMode },
+		{ mods = "CTRL", key = "x", action = act.ActivateCopyMode },
 		{ mods = mod, key = "s", action = act.ShowTabNavigator },
 		{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("PrimarySelection") },
 		{ key = "v", mods = "SUPER", action = wezterm.action.PasteFrom("Clipboard") },
 	},
 	use_dead_keys = false,
+	key_tables = {
+		copy_mode = {
+			{
+				key = "e",
+				mods = "NONE",
+				action = act.CopyMode("MoveForwardWordEnd"),
+			},
+		},
+	},
+	max_fps = 120,
+	tab_max_width = 60,
 	bold_brightens_ansi_colors = true,
 	window_background_opacity = 0.95,
 	cell_width = 0.9,
