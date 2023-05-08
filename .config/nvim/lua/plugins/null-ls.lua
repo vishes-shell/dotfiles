8b33a6ffcc61ca45b24fbdbf3e14d16c1ca720b6
Aleksei Shalynin <aleksei.shalynin@Shalynin-Heart.local>
2023-05-08 13:14:37 +0300

    Reenable gq in nvim
    See jose-elias-alvarez/null-ls.nvim#1131

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -64,6 +64,9 @@ return {
             },
           }),
         },
+        on_attach = function(_, bufnr)
+          vim.api.nvim_buf_set_option(bufnr, "formatexpr", "")
+        end,
       }
     end,
   },
