d2d3f9b9b2b768a66826d5ed386828d5506b3f9a
Aleksei Shalynin <aleksei.shalynin@Shalynin-Heart.local>
2023-05-11 22:10:27 +0300

    Add telescope frecency

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,13 @@ return {
           require("telescope").load_extension("fzf")
         end,
       },
+      {
+        "nvim-telescope/telescope-frecency.nvim",
+        config = function()
+          require("telescope").load_extension("frecency")
+        end,
+        dependencies = { "kkharji/sqlite.lua" },
+      },
     },
     keys = {
       { "<leader>tt", "<cmd>Telescope tags<cr>", desc = "Tags" },
