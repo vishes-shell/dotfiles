0a8453be1f9e280faae037bbafca705b12a5a4ef
Aleksei Shalynin <aleksei.shalynin@Shalynin-Heart.local>
2023-05-15 08:02:18 +0300

    Remove lsp_saga

--- .config/nvim/lua/plugins/code.lua
+++ .config/nvim/lua/plugins/code.lua
@@ -136,16 +136,4 @@ return {
       },
     },
   },
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    config = function()
-      require("lspsaga").setup({})
-    end,
-    dependencies = {
-      { "nvim-tree/nvim-web-devicons" },
-      --Please make sure you install markdown and markdown_inline parser
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-  },
 }
