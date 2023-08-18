e2c569e035d998efcb8a6aa2c6401bd1acd2e15a
Aleksei Shalynin <aleksei.shalynin@ShalyninA.local>
2023-08-18 16:18:13 +0300

    Remove zsh

--- .fzf.zsh
+++
@@ -1,3 +0,0 @@
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -l -g ""'
-export FZF_CTRL_T_COMMAND='ag --hidden --ignore .git -l -g ""'
-export FZF_ALT_C_COMMAND='ag --hidden --ignore .git -l -g ""'
--- zsh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
