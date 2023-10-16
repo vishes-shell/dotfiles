a1b25786fc4c7b6184866977ae9c51d154ff1423
Aleksei Shalynin <aleksei.shalynin@ShalyninA.local>
2023-10-16 18:24:47 +0300

    change alpha to dashboard in nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,41 +1,32 @@
 return {
-  -- welcome window
   {
-    "goolord/alpha-nvim",
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[
-  _   _                     _                                                    
- | |_| |__   ___ _ __ ___  (_)___   _ __   ___     ___  ___  ___ __ _ _ __   ___ 
- | __| '_ \ / _ \ '__/ _ \ | / __| | '_ \ / _ \   / _ \/ __|/ __/ _` | '_ \ / _ \
- | |_| | | |  __/ | |  __/ | \__ \ | | | | (_) | |  __/\__ \ (_| (_| | |_) |  __/
-  \__|_| |_|\___|_|  \___| |_|___/ |_| |_|\___/   \___||___/\___\__,_| .__/ \___|
-                                                                     |_|         
-      ]]
-      dashboard.section.header.val = vim.split(logo, "\n")
-      dashboard.section.buttons.val = {
-        dashboard.button("n", " " .. " New file", [[:lua require("genghis").createNewFile() <cr>]]),
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("/", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button(
-          "z",
-          " " .. " New Note",
-          [[:lua vim.ui.input({prompt="Title"}, function(title) if title then require("zk").new({title=title}) end end) <cr>]]
-        ),
-        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-      }
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-      dashboard.section.footer.opts.hl = "Type"
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.opts.layout[1].val = 8
-      return dashboard
-    end,
+    "nvimdev/dashboard-nvim",
+    opts = {
+      config = {
+        header = vim.split(string.rep("\n", 8) .. [[
+            _   _                     _                                                    
+           | |_| |__   ___ _ __ ___  (_)___   _ __   ___     ___  ___  ___ __ _ _ __   ___ 
+           | __| '_ \ / _ \ '__/ _ \ | / __| | '_ \ / _ \   / _ \/ __|/ __/ _` | '_ \ / _ \
+           | |_| | | |  __/ | |  __/ | \__ \ | | | | (_) | |  __/\__ \ (_| (_| | |_) |  __/
+            \__|_| |_|\___|_|  \___| |_|___/ |_| |_|\___/   \___||___/\___\__,_| .__/ \___|
+                                                                                |_|        
+          ]] .. "\n\n", "\n"),
+        center = {
+          { action = 'lua require("genghis").createNewFile()', desc = "New file", icon = " ", key = "n" },
+          { action = "Telescope find_files", desc = "Find file", icon = " ", key = "f" },
+          { action = "Telescope live_grep", desc = "Find text", icon = " ", key = "/" },
+          {
+            action = 'lua vim.ui.input({prompt="Title"}, function(title) if title then require("zk").new({title=title}) end end)',
+            desc = "New note",
+            icon = " ",
+            key = "z",
+          },
+          { action = "Telescope oldfiles", desc = "Recent files", icon = " ", key = "r" },
+          { action = "Lazy", desc = "Lazy", icon = "鈴", key = "l" },
+          { action = "qa", desc = "Quit", icon = " ", key = "q" },
+        },
+      },
+    },
   },
   {
     "folke/noice.nvim",
@@ -52,6 +43,7 @@ return {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
+        inc_rename = true,
       },
     },
   },
