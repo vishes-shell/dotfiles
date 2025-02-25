675fc4a2ddd21169b7ca7029ec99704d73c505ed
Aleksei Shalynin <aleksei.shalynin@ShalyninA.local>
2025-02-25 11:14:06 +0300

    setup codecompanion

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -30,14 +30,43 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+    lazy = false,
     config = function()
       require("codecompanion").setup({
-        adapters = {},
+        adapters = {
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              url = "https://api.proxyapi.ru/anthropic/v1/messages",
+              env = {
+                api_key = "CODE_COMPANION_ANTHROPIC_API_KEY",
+              },
+            })
+          end,
+          openai = function()
+            return require("codecompanion.adapters").extend("openai", {
+              url = "https://api.proxyapi.ru/openai/v1/chat/completions",
+              env = {
+                api_key = "CODE_COMPANION_OPENAI_API_KEY",
+              },
+              schema = { model = { default = "gpt-4o-mini" } },
+            })
+          end,
+          deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              url = "https://api.proxyapi.ru/deepseek/chat/completions",
+              env = {
+                api_key = "CODE_COMPANION_DEEPSEEK_API_KEY",
+              },
+              schema = { model = { default = "deepseek-chat" } },
+            })
+          end,
+        },
         strategies = {
-          chat = { adapter = "anthropic" },
-          inline = { adapter = "anthropic" },
+          chat = { adapter = "openai" },
+          inline = { adapter = "opanai" },
         },
       })
     end,
+    keys = { { "<leader>cc", "<Cmd>CodeCompanionChat Toggle<CR>", desc = "Code Companion Chat" } },
   },
 }
