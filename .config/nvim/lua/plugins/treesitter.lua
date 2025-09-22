dc456b8f28c523deaa28afb27e6af8903c75cd0e
Aleksei Shalynin <aleksei.shalynin@ShalyninA.local>
2025-09-22 14:11:30 +0300

    Remove deprecated incremental selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -41,16 +41,6 @@ return {
         default_lazy = true,
         default_fallback = "auto",
       },
-      incremental_selection = {
-        enable = true,
-        disable = {},
-        keymaps = {
-          init_selection = "<CR>", -- maps in normal mode to init the node/scope selection
-          node_incremental = "<CR>", -- increment to the upper named parent
-          scope_incremental = "<nop>", -- increment to the upper scope (as defined in locals.scm)
-          node_decremental = "<BS>", -- decrement to the previous node
-        },
-      },
       query_linter = {
         enable = true,
         use_virtual_text = true,
