60ad515d7bf44f6934e401d0ad88628ee147202c
Aleksei Shalynin <aleksei.shalynin@ShalyninA.local>
2025-11-17 16:14:47 +0300

    Remove postgres from fish path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,6 @@ fish_add_path /usr/sbin
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-fish_add_path /usr/local/opt/postgresql@11/bin
 fish_add_path /opt/homebrew/opt/gnu-sed/libexec/gnubin
 fish_add_path ~/.opencode/bin
 fish_add_path ~/.bun/bin
