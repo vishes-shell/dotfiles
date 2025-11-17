ff189739681004883af01495d7548c8901b7835c
Aleksei Shalynin <aleksei.shalynin@ShalyninA.local>
2025-11-17 16:14:57 +0300

    Fix finicky config

--- .finicky.js
+++ .finicky.js
@@ -1,22 +1,23 @@
 // ~/.finicky.js
 
-module.exports = {
+export default {
   defaultBrowser: "/Applications/Firefox Developer Edition.app",
+
   rewrite: [
     {
-      // Redirect all urls to use https
-      match: ({ url }) => url.protocol === "http",
-      url: { protocol: "https" },
+      match: ({ url }) => url.protocol === "http:",
+      url: ({ url }) => ({ ...url, protocol: "https:" }),
     },
   ],
+
   handlers: [
     {
       match: ["meet.google.com/*", "edu.livedigital.space/*"],
       browser: "Google Chrome",
     },
   ],
+
   options: {
-    // Hide the finicky icon from the top bar. Default: false
     hideIcon: true,
   },
 };
